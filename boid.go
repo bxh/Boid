d0f6e8977db3b1ec28056925806a77b3d3a501b4
Bangxin Hu <hu.bangxin@gmail.com>
2020-09-27 15:59:43 -0400

    Add mutex

--- boid.go
+++ boid.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math"
 	"math/rand"
 	"time"
 )
@@ -12,7 +13,14 @@ type Boid struct {
 }
 
 func (b *Boid) moveOne() {
+	acc := b.accelerate()
+	rwLock.Lock()
+	b.velocity = b.velocity.Add(acc).Limit(-1, 1)
+	boidMap[int(b.position.x)][int(b.position.y)] = -1
 	b.position = b.position.Add(b.velocity)
+	boidMap[int(b.position.x)][int(b.position.y)] = b.id
+	rwLock.Unlock()
+
 	next := b.position.Add(b.velocity)
 	if next.x >= screenWidth || next.x < 0 {
 		b.velocity = Vector2D{-b.velocity.x, b.velocity.y}
@@ -20,6 +28,49 @@ func (b *Boid) moveOne() {
 	if next.y >= screenHeight || next.y < 0 {
 		b.velocity = Vector2D{b.velocity.x, -b.velocity.y}
 	}
+}
+
+func (b *Boid) accelerate() Vector2D {
+	upper, lower := b.position.AddV(viewRadius), b.position.AddV(-viewRadius)
+	avgVelocity := Vector2D{0, 0}
+	avgPosition := Vector2D{0, 0}
+	separation := Vector2D{0, 0}
+	count := 0.0
+
+	rwLock.RLock() // NOTE: The mutexes in GO are not re-entrant.
+	for i := math.Max(lower.x, 0); i < math.Min(upper.x, screenWidth); i++ {
+		for j := math.Max(lower.y, 0); j < math.Min(upper.y, screenHeight); j++ {
+			if otherBoid := boidMap[int(i)][int(j)]; otherBoid != -1 && otherBoid != b.id {
+				if dist := boids[otherBoid].position.Distance(b.position); dist < viewRadius {
+					count++
+					avgVelocity = avgVelocity.Add(boids[otherBoid].velocity)
+					avgPosition = avgPosition.Add(boids[otherBoid].position)
+					separation = separation.Add(b.position.Subtract(boids[otherBoid].position).DivisionV(dist))
+				}
+			}
+		}
+	}
+	rwLock.RUnlock()
+
+	acc := Vector2D{b.borderBounce(b.position.x, screenWidth), b.borderBounce(b.position.y, screenHeight)}
+	if count > 0 {
+		avgPosition, avgVelocity = avgPosition.DivisionV(count), avgVelocity.DivisionV(count)
+		accAlignment := avgVelocity.Subtract(b.velocity).MultiplyV(adjustionRate)
+		accCohesion := avgPosition.Subtract(b.position).MultiplyV(adjustionRate)
+		accSeparation := separation.MultiplyV(adjustionRate)
+		acc = accAlignment.Add(accCohesion).Add(accSeparation)
+	}
+
+	return acc
+}
+
+func (b *Boid) borderBounce(position, max float64) float64 {
+	if position < viewRadius {
+		return 1 / position
+	} else if position > max-viewRadius {
+		return 1 / (position - max)
+	}
+	return 0
 }
 
 func (b *Boid) start() {
@@ -36,5 +87,6 @@ func createBoid(bid int) {
 		id:       bid,
 	}
 	boids[bid] = &b
+	boidMap[int(b.position.x)][int(b.position.y)] = bid
 	go b.start()
 }
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"image/color"
 	"log"
+	"sync"
 
 	"github.com/hajimehoshi/ebiten"
 )
@@ -10,11 +11,15 @@ import (
 const (
 	screenWidth, screenHeight = 640, 360
 	boidCount                 = 300
+	viewRadius                = 13
+	adjustionRate             = .015
 )
 
 var (
-	green = color.RGBA{10, 255, 50, 255}
-	boids [boidCount]*Boid
+	green   = color.RGBA{10, 255, 50, 255}
+	boids   [boidCount]*Boid
+	boidMap [screenWidth + 1][screenHeight + 1]int
+	rwLock  = sync.RWMutex{}
 )
 
 func update(screen *ebiten.Image) error {
@@ -30,6 +35,12 @@ func update(screen *ebiten.Image) error {
 }
 
 func main() {
+	for i, row := range boidMap {
+		for j := range row {
+			boidMap[i][j] = -1
+		}
+	}
+
 	for i := 0; i < boidCount; i++ {
 		createBoid(i)
 	}
